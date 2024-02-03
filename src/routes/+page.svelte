bcc8564088408176b2fab9feaebfa09490c7a52e
gabrycina <c.gabriele.info@gmail.com>
2024-02-03 18:32:54 +0000

    Add form in /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+  import PrimaryButton from "$lib/components/inputs/PrimaryButton.svelte";
+  import Textarea from "$lib/components/inputs/Textarea.svelte";
+</script>
+
+<div class="bg-white">
+  <div class="mx-auto max-w-2xl py-16 px-6 sm:py-80 lg:px-8">
+    <form action="" class="text-center flex flex-col space-y-2">
+      <Textarea name="prompt" label="describe your next animation ✨" />
+      <div class="text-end">
+        <PrimaryButton>generate</PrimaryButton>
+      </div>
+    </form>
+  </div>
+</div>
