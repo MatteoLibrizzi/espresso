ab3d9a73eee78172436f6bac17928d23e936b1e7
gabrycina <c.gabriele.info@gmail.com>
2024-02-03 18:36:35 +0000

    Fix form ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,12 @@
 <div class="bg-white">
   <div class="mx-auto max-w-2xl py-16 px-6 sm:py-80 lg:px-8">
     <form action="" class="text-center flex flex-col space-y-2">
-      <Textarea name="prompt" label="describe your next animation ✨" />
+      <Textarea
+        name="prompt"
+        label="describe your next animation ✨"
+        rows="8"
+        placeholder="I want to see a 3x3 matrix with numbers from 1 to 9, then I want to see that matrix multiplied by itself transposed"
+      />
       <div class="text-end">
         <PrimaryButton>generate</PrimaryButton>
       </div>
