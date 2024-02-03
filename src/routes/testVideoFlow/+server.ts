7685da8e3fa7170f7aafe6e9fb4d88dc758e1ffc
MatteoLibrizzi <librizzimatteo.ml@gmail.com>
2024-02-03 23:59:22 +0000

    Fixed post

--- src/routes/testVideoFlow/+server.ts
+++ src/routes/testVideoFlow/+server.ts
@@ -1,25 +1,11 @@
 import { SESSION } from '../../services/storage'
-import UserFlowHandler from '../../services/userFlowHandler'
-
-const conv = new UserFlowHandler()
-
-export async function GET(req) {
-	const prompt = req.url.searchParams.get('query')
-	await conv.nextVideo(prompt!, '')
-
-	return new Response(JSON.stringify(conv) as any, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	})
-}
 
 export async function POST(req) {
 	const body = await req.request.json()
 	const prompt = body.prompt
 	const codeBlock = body.code
 
-	console.log(await conv.nextVideo(prompt!, codeBlock ?? ''))
+	console.log(await SESSION.nextVideo(prompt!, codeBlock ?? ''))
 
 	const conversation = SESSION.getCompleteConversation()
 
