83ebfae150d32315437f9298d4d12f1a4f09a5e4
gabrycina <c.gabriele.info@gmail.com>
2024-02-03 16:21:19 +0000

    Fix gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   kit: {
     adapter: adapter(),
   },
