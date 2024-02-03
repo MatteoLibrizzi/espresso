6b7f6eb2c9b255cffcf3118a38f79f56c4d53238
Tom Ursus Kosteran <tuk@example.com>
2024-02-03 18:55:12 +0000

    Working gpt

--- src/routes/testGpt/+server.ts
+++ src/routes/testGpt/+server.ts
@@ -1,11 +1,13 @@
 import ConversationHandler from "../../services/conversationHandler";
 
-export function GET() {
-    console.log(process.env.OPENAI_KEY);
-    const ch = new ConversationHandler();
-    const res = ch.makeCall("give me your name");
-
-    return new Response(res as any, {
+export async function GET() {
+    const ch = new ConversationHandler(import.meta.env.VITE_OPENAI_KEY);
+    const res = await ch.nextQuery(
+        "I want to see a 3x3 matrix with numbers from 1 to 9, then I want to see that matrix multiplied by itself transposed"
+    );
+    const res2 = await ch.nextQuery("now 4x4");
+    const conv = ch.getConversationHistory();
+    return new Response(JSON.stringify(conv) as any, {
         headers: {
             "Content-Type": "application/json",
         },
