ab217607d168adb9741556d6d61596011bd9555c
gabrycina <c.gabriele.info@gmail.com>
2024-02-03 20:29:09 +0000

    Add form validation and toasts

---
+++ src/lib/utils/validator.ts
@@ -0,0 +1,63 @@
+import {
+  Type,
+  type Static,
+  type TObject,
+  type TUnion,
+  type TLiteral,
+  type SchemaOptions,
+} from "@sinclair/typebox";
+import Ajv, { type ErrorObject } from "ajv";
+import addFormats from "ajv-formats";
+import addErrors from "ajv-errors";
+import set from "set-value";
+
+const ajv = new Ajv({ allErrors: true, coerceTypes: true, useDefaults: true });
+addFormats(ajv, { keywords: true });
+addErrors(ajv);
+
+export class ValidationError extends Error {
+  errors: Array<{ path: string; message: string }>;
+  constructor(errors: Array<ErrorObject>) {
+    super();
+
+    this.errors = errors.map(({ instancePath, message }) => ({
+      path: instancePath ?? "",
+      message: message ?? "",
+    }));
+  }
+}
+
+export function compileValidator<T extends TObject>(schema: T) {
+  const validate = (data: FormData | URLSearchParams) => {
+    const validator = ajv.compile(schema);
+    const object = {};
+    for (const key of data.keys()) {
+      const values = data.getAll(key);
+      if (values.length === 1) {
+        set(object, key, values[0]);
+      } else {
+        set(object, key, values);
+      }
+    }
+
+    if (validator(object)) {
+      return object as Static<T>;
+    } else {
+      throw new ValidationError(validator.errors ?? []);
+    }
+  };
+
+  return validate;
+}
+
+type IntoStringLiteralUnion<T> = {
+  [K in keyof T]: T[K] extends string ? TLiteral<T[K]> : never;
+};
+
+export function StringLiteralUnion<T extends string[]>(
+  values: readonly [...T],
+  options?: SchemaOptions
+): TUnion<IntoStringLiteralUnion<T>> {
+  const literals = values.map((value) => Type.Literal(value));
+  return Type.Union(literals, options) as TUnion<IntoStringLiteralUnion<T>>;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,38 @@
-<script>
+<script lang="ts">
+  import { applyAction, enhance } from "$app/forms";
   import PrimaryButton from "$lib/components/inputs/PrimaryButton.svelte";
   import Textarea from "$lib/components/inputs/Textarea.svelte";
+  import toast from "svelte-french-toast";
+  import type { SubmitFunction } from "@sveltejs/kit";
+
+  const onSubmit: SubmitFunction = () => {
+    return async ({ result, update }) => {
+      switch (result.type) {
+        case "success":
+          toast.success("Done!");
+          await update();
+          break;
+        case "error":
+          toast.error(result.error.message);
+          break;
+        case "failure":
+          toast.error(result.data?.errors.join("\n") ?? [].join("\n"));
+          break;
+        default:
+          await applyAction(result);
+          break;
+      }
+    };
+  };
 </script>
 
 <div class="bg-white">
   <div class="mx-auto max-w-2xl py-16 px-6 sm:py-80 lg:px-8">
-    <form action="" class="text-center flex flex-col space-y-2">
+    <form
+      method="POST"
+      use:enhance={onSubmit}
+      class="text-center flex flex-col space-y-2"
+    >
       <Textarea
         name="prompt"
         label="describe your next animation ✨"
