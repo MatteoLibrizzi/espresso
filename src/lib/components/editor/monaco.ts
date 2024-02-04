fac5f5bf9b50de37f53d9708e165e293fc9e7c2b
gabrycina <c.gabriele.info@gmail.com>
2024-02-04 07:58:33 +0000

    Remove unused imports

--- src/lib/components/editor/monaco.ts
+++ src/lib/components/editor/monaco.ts
@@ -1,17 +1,9 @@
 import * as monaco from "monaco-editor";
 
-// Import the workers in a production-safe way.
-// This is different than in Monaco's documentation for Vite,
-// but avoids a weird error ("Unexpected usage") at runtime
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
-import { Registry } from "monaco-textmate"; // import the Registry class from monaco-textmate
-import { wireTmGrammars } from "monaco-editor-textmate"; // import the wireTmGrammars function
-import * as pythonGrammar from "./MagicPython.tmLanguage.json";
-// import * as darkTheme from "./vs-dark-plus-theme.json";
+import { Registry } from "monaco-textmate";
 
 self.MonacoEnvironment = {
   getWorker: function (_: string, label: string) {
