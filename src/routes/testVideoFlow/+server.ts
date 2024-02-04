68265086d8d59677a7bdc8cc8c040c7b48e916c2
MatteoLibrizzi <librizzimatteo.ml@gmail.com>
2024-02-04 00:02:27 +0000

    Fix post for real though

--- src/routes/testVideoFlow/+server.ts
+++ src/routes/testVideoFlow/+server.ts
@@ -5,9 +5,10 @@ export async function POST(req) {
 	const prompt = body.prompt
 	const codeBlock = body.code
 
-	console.log(await SESSION.nextVideo(prompt!, codeBlock ?? ''))
-
-	const conversation = SESSION.getCompleteConversation()
+	const { conversation } = await SESSION.nextVideo(
+		prompt!,
+		codeBlock ?? ''
+	)
 
 	return new Response(JSON.stringify({ conversation }) as string, {
 		headers: {
