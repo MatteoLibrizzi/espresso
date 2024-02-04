c9b9e5012d6d74e87db8fb41652d97e946787ebc
MatteoLibrizzi <librizzimatteo.ml@gmail.com>
2024-02-04 02:22:06 +0000

    Fixed broken frontend

--- src/lib/components/VideoAndCodeContainer.svelte
+++ src/lib/components/VideoAndCodeContainer.svelte
@@ -1,9 +1,9 @@
 <script>
-// @ts-nocheck
+	// @ts-nocheck
 
 	import VideoPlayer from './VideoPlayer.svelte'
 
-	import { AceEditor } from 'svelte-ace'
+
 	export let videoSource = ''
 	export let code = ''
 
@@ -22,17 +22,4 @@
 		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 		>Show code</button
 	>
-	{#if showCode}
-		<AceEditor
-			theme="monokai"
-			aria-hidden="false"
-			width="100%"
-			height="200px"
-			mode="python"
-			value={code}
-			readonly={false}
-			class="w-96"
-			lineNumbers={false}
-		/>
-	{/if}
 </main>
