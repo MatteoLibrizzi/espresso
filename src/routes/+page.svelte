0a0f8426c48c70d7263cbb5db5fcad8243b66057
gabrycina <c.gabriele.info@gmail.com>
2024-02-04 01:03:16 +0000

    Add alignment for different roles and code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,39 @@
 <script lang="ts">
   import PrimaryButton from "$lib/components/inputs/PrimaryButton.svelte";
   import InteractionTile from "$lib/components/layout/InteractionTile.svelte";
-	import VideoPlayer from '$lib/components/VideoPlayer.svelte'
-	import type { Interaction } from "../services/gptInterface"
+  import VideoPlayer from "$lib/components/VideoPlayer.svelte";
+  import type { Interaction } from "../services/gptInterface";
 
-	$: conversation = [] as Interaction[]
+  $: conversation = [] as Interaction[];
 
-	let newPrompt = ''
-	let div: HTMLDivElement
+  let newPrompt = "";
+  let div: HTMLDivElement;
 
-	const submitPrompt = async () => {
-		console.log('submitting prompt', newPrompt)
-		const response = await fetch('/testVideoFlow', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ prompt: newPrompt }),
-		})
+  const submitPrompt = async () => {
+    console.log("submitting prompt", newPrompt);
+    const response = await fetch("/testVideoFlow", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: newPrompt }),
+    });
 
-		const data = await response.json()
+    const data = await response.json();
 
-		conversation = [...data.conversation]
-		newPrompt = ''
-	}
+    conversation = [...data.conversation];
+    newPrompt = "";
+  };
 </script>
 
 <svelte:head>
-	<title>Espresso</title>
+  <title>Espresso</title>
 </svelte:head>
 
 <div
-	class="bg-gray-50 flex flex-col items-center justify-between min-h-screen"
-	bind:this={div}
+  class="bg-gray-50 flex flex-col items-center justify-between min-h-screen"
+  bind:this={div}
 >
-
   <div
     id="chat-container"
     class="max-h-400 overflow-y-auto w-full mx-auto max-w-3xl"
@@ -52,12 +51,12 @@
     </ul>
   </div>
 
-	<div class="flex flex-row gap-4 p-5 mt-auto max-w-3xl w-full">
-		<textarea
-			class="flex-grow p-2 border rounded-md"
-			bind:value={newPrompt}
-			placeholder="I want to see a 3x3 matrix with numbers from 1 to 9, then I want to see that matrix multiplied by itself transposed"
-		></textarea>
-		<PrimaryButton on:click={submitPrompt}>✨</PrimaryButton>
-	</div>
+  <div class="flex flex-row gap-4 p-5 mt-auto max-w-3xl w-full">
+    <textarea
+      class="flex-grow p-2 border rounded-md"
+      bind:value={newPrompt}
+      placeholder="I want to see a 3x3 matrix with numbers from 1 to 9, then I want to see that matrix multiplied by itself transposed"
+    ></textarea>
+    <PrimaryButton on:click={submitPrompt}>✨</PrimaryButton>
+  </div>
 </div>
