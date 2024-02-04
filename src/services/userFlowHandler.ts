59d852a5630abd4c8c9711e97bffce45cd2847ac
Tom Ursus Kosteran <tuk@example.com>
2024-02-04 01:30:51 +0000

    remove errors

--- src/services/userFlowHandler.ts
+++ src/services/userFlowHandler.ts
@@ -1,8 +1,8 @@
 import ConversationHandler from "./conversationHandler";
 import type { Interaction } from "./gptInterface";
 import { video_render } from "./utilities";
-import { writeFile, readFile} from "fs/promises";
-import {createHash} from "crypto";
+import { writeFile, readFile } from "fs/promises";
+import { createHash } from "crypto";
 
 function getFixPrompt(prompt: string, error: string) {
     return `The returned code for "${prompt} returned compile error ${error}, fix it`;
@@ -39,6 +39,11 @@ function cleanConversation(conversation: Interaction[]): Interaction[] {
                 videoPath: conversation[i + 1].content,
             });
             i++; // Skip next interaction
+        } else if (
+            conversation[i].role === "user" &&
+            conversation[i].content.startsWith("The returned code for ")
+        ) {
+            i++; // Skip this interaction and the next one
         } else {
             cleanedConversation.push(conversation[i]);
         }
@@ -55,13 +60,12 @@ class UserFlowHandler {
     }
 
     async nextVideo(prompt: string, codeBlock: string) {
-
-        const fileName = `store/${createHash('sha256').update(prompt).digest('hex')}`;
+        const fileName = `store/${createHash("sha256").update(prompt).digest("hex")}`;
         console.log(`The filename is: ${fileName}`);
-        try{
+        try {
             const content = await readFile(fileName);
             console.log("Returned cached data");
-            return {conversation: JSON.parse(content.toString())};
+            return { conversation: JSON.parse(content.toString()) };
         } catch (error) {}
 
         if (
@@ -91,13 +95,15 @@ class UserFlowHandler {
             role: "videoPath",
             content: videoPath,
         });
-        
-        const compleConversation = cleanConversation(this.getCompleteConversation());
+
+        const compleConversation = cleanConversation(
+            this.getCompleteConversation()
+        );
         this.remember(fileName, compleConversation);
-        return { conversation: compleConversation};
+        return { conversation: compleConversation };
     }
 
-    async remember(fileName: string, compleConversation: Interaction[]){
+    async remember(fileName: string, compleConversation: Interaction[]) {
         await writeFile(fileName, JSON.stringify(compleConversation));
     }
 
