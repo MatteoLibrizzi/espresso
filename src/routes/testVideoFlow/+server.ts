60ad0beedb9522b4630936fd805b14f70d5bedf1
calebgcc <calebgucciardi@gmail.com>
2024-02-04 00:16:38 +0000

    added two logs

--- src/routes/testVideoFlow/+server.ts
+++ src/routes/testVideoFlow/+server.ts
@@ -3,8 +3,10 @@ import UserFlowHandler from "../../services/userFlowHandler";
 const conv = new UserFlowHandler();
 
 export async function GET(req) {
-    const prompt = req.url.searchParams.get("query");
-    console.log(await conv.nextVideo(prompt!));
+    const prompt = req.url.searchParams.get("prompt");
+    const code = req.url.searchParams.get("code");
+    console.log(`${prompt}`);
+    console.log(await conv.nextVideo(prompt!, code!));
 
     return new Response(JSON.stringify(conv) as any, {
         headers: {
